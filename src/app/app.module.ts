713f8626d7a9cbf184fe3677ad37a5769c7b6641
Achrafvin <achraf.derkaoui@emsi-edu.ma>
2023-12-16 00:51:10 +0100

    AddHero

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeroSectionComponent } from './components/hero-section/hero-section.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    HeroSectionComponent
   ],
   imports: [
     BrowserModule,
