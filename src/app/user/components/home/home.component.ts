df7f90e30102a450a4514aed4c9b48034b189720
adyatar <119011457+adyatar@users.noreply.github.com>
2023-12-22 00:16:49 +0100

    derkaz

--- src/app/user/components/home/home.component.ts
+++ src/app/user/components/home/home.component.ts
@@ -1,8 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { SlickCarouselModule } from 'ngx-slick-carousel';
+import { SharedModule } from '../../shared/shared.module';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
+  standalone:true,
+  imports:[CommonModule,
+    SlickCarouselModule,
+    SharedModule,RouterModule,],
+    
 })
 export class HomeComponent{
  
