9dc6a35639bfdce7ceacd735ad2644ac53dae752
Achrafvin <achraf.derkaoui@emsi-edu.ma>
2024-01-10 01:15:36 +0100

    Ach

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 import { filter } from 'rxjs/operators';
-import { AuthService } from './services/Security/auth.service';
 
 @Component({
   selector: 'app-root',
